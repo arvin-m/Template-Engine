e15b352208637af0a91c7d0d57256380bc13495b
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-02-14 12:41:52 -0500

    adding promt questions for enginnieer and intern and create new object for each one

--- app.js
+++ app.js
@@ -8,75 +8,257 @@ const Intern = require("./lab/intern.js");
 
 
 
-function appBody(){
+function appBody() {
+    console.log("Please create Your Team ")
 
-    function createanager(){
+    function createanager() {
 
         inquirer
-        .prompt([{
-            type: "input",
-            name: "managerName",
-            message: "What is your manager's name? ",
-            validate: answer => {
-              if (answer !== "") {
-                return true;
-              }
-              return "Please enter valid string character.";
-            }
-          },
+            .prompt([{
+                type: "input",
+                name: "managerName",
+                message: "What is your manager's name? ",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter valid string character.";
+                }
+            },
             {
                 type: "number",
                 name: "managerId",
                 message: "What is your manager's id? ",
                 validate: answer => {
-                
-                  if (answer === Number || answer > 0) {
-                    return true;
-                  }
-                  return "Please enter a NUMBER and GRATER than zero.";
+
+                    if (answer === Number || answer > 0) {
+                        return true;
+                    }
+                    return "Please enter a NUMBER and GRATER than zero.";
                 }
-              },
-              {
+            },
+            {
                 type: "email",
                 name: "managerEmail",
                 message: "What is your manager's VALID Email ? ",
                 validate: Email => {
-                
-                  if (Email !== "" ) {
-                    return true;
-                  }
-                  return "Please enter a VALID Email address!";
+
+                    if (Email !== "") {
+                        return true;
+                    }
+                    return "Please enter a VALID Email address!";
                 }
-              },{
+            },
+             {
                 type: "number",
                 name: "officeNumber",
                 message: "What is your manager's office number ? ",
                 // validate: officeNum => {
-                
+
                 //   if (officeNum === Number ) {
                 //     return true;
                 //   }
                 //   return "Please enter a NUMBER!";
                 // }
-              },
+            },
+
+
+            ])
+            .then(answers => {
+                const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.officeNumber);
+                console.log(manager);
+                createTeamMember()
+                
+            });
+
+           
+     
+    }
+    
+// ------------------------------------------------------------------------------------
+    function createTeamMember(){
+        inquirer
+        .prompt([
+            {
+                type:"list",
+                name:"createTeamMember",
+                message:"Wich type of team member would you like to add ? ",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "I don't want to add any more team members"
+                  ]
+            }
+        ])
+        .then(choices =>{
+            switch(choices.createTeamMember){
+                case "Engineer":
+                    createEanageer();
+                    break;
+                case "Intern":
+                    createIntern();
+                    break;
+                default: generateHTML();   
             
-
-          ])
-          .then(answers => {
-            const manager= new Manager(answers.managerName,answers.managerId,answers.managerEmail,answers.officeNumber);
-            console.log(manager);
-          });
+            }
+            
+        })
 
 
 
 
-        }
+}
+
+// ------------------------------------------------------------------------------------
+    function createEanageer(){
         
-        
-        createanager()
+        inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "eanageerName",
+                message: "What is your Eanageer's name? ",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter valid string character.";
+                }
+            },
+            {
+                type: "number",
+                name: "eanageerId",
+                message: "What is your Eanageer's id? ",
+                validate: answer => {
+
+                    if (answer === Number || answer > 0) {
+                        return true;
+                    }
+                    return "Please enter a NUMBER and GRATER than zero.";
+                }
+            },
+            {
+                type: "email",
+                name: "eanageerEmail",
+                message: "What is your Eanageer's VALID Email ? ",
+                validate: Email => {
+
+                    if (Email !== "") {
+                        return true;
+                    }
+                    return "Please enter a VALID Email address!";
+                }
+            },
+            {
+                type: "input",
+                name: "eanageerGitHub",
+                message: "What is your Eanageer's GitHub user name ? ",
+                validate: github => {
+
+                    if (github !== "") {
+                        return true;
+                    }
+                    return "Please enter the GitHub user name!";
+                }
+            },
+        ])
+        .then( answers =>{
+            const engineer= new Engineer(answers.eanageerName,answers.eanageerId,answers.eanageerEmail,answers.eanageerGitHub);
+            console.log(engineer);
+            createTeamMember();
+
+
+        })
 
 
 
+
+
+    };
+
+// ------------------------------------------------------------------------------------
+    function createIntern(){
+        inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "What is your intern's name? ",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter valid string character.";
+                }
+            },
+            {
+                type: "number",
+                name: "internId",
+                message: "What is your intern's id? ",
+                validate: answer => {
+
+                    if (answer === Number || answer > 0) {
+                        return true;
+                    }
+                    return "Please enter a NUMBER and GRATER than zero.";
+                }
+            },
+            {
+                type: "email",
+                name: "internEmail",
+                message: "What is your intern's VALID Email ? ",
+                validate: Email => {
+
+                    if (Email !== "") {
+                        return true;
+                    }
+                    return "Please enter a VALID Email address!";
+                }
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "What is your intern's school name ? ",
+                validate: github => {
+
+                    if (school !== "") {
+                        return true;
+                    }
+                    return "Please enter the school  name!";
+                }
+            },
+        ])
+        .then( answers =>{
+            const intern= new Intern(answers.internName,answers.internId,answers.internEmail,answers.internSchool);
+            console.log(engineer);
+            createTeamMember();
+
+
+        })
+
+
+
+
+
+
+
+
+    };
+
+// ------------------------------------------------------------------------------------
+    function generateHTML(){
+
+
+
+
+
+    }
+
+
+
+
+
+    createanager()
 
 
 
@@ -88,4 +270,3 @@ function appBody(){
 
 appBody();
 
-    
